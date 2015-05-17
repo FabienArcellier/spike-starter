efa6a7178fbd122fd9a72a5a8ac054f89c05c96c
Fabien Arcellier <farcellier@octo.com>
2015-05-17 11:58:47 +0000

    PROJECT ID CALCULATE USING CURRENT DATE TIME
    I use the datetime to calculate the project id. It's important
    to have them easily sorted.
    
    Use format in place of old printf syntax for python

--- spike-starter.py
+++ spike-starter.py
@@ -3,27 +3,33 @@
 import sys
 import os
 import git
+from datetime import datetime
 
 def main():
     try:
-        # lire le nom du projet
-        project_id = sys.argv[1]
-        project_name = sys.argv[2]
+        # Read project name from command line
+        project_name = sys.argv[1]
 
-        # creer un dossier
-        project_directory = os.path.abspath("%s_%s" % (project_id, project_name))
-        logDebug("project_directory: %s" % project_directory)
+        # project id from today date
+        now = datetime.now()
+        project_id = "{0:d}{1:0>2d}{2:0>2d}_{3:0>2d}{4:0>2d}".format(now.year, now.month, now.day, now.hour, now.minute)
+        logDebug("project_id: {}".format(project_id))
+
+
+        # Create project directory
+        project_directory = os.path.abspath("{}__{}".format(project_id, project_name))
+        logDebug("project_directory: {}".format(project_directory))
 
         if not os.path.exists(project_directory):
             os.makedirs(project_directory)
-            logInformation("PROJECT DIRECTORY : %s [OK]" % project_directory)
+            logInformation("PROJECT DIRECTORY : {} [OK]".format(project_directory))
         else:
             logAlert("Project already exits %s" % project_directory)
             sys.exit(1)
 
-        # Creer un depot git
+        # Initialize git repository
         project_repository = git.Repo.init(project_directory)
-        logInformation("PROJECT GIT REPOSITORY : %s [OK]" % project_directory)
+        logInformation("PROJECT GIT REPOSITORY : {} [OK]".format(project_directory))
 
         # Creer un fichier .gitignore
 
