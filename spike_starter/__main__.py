3f3c8c85872424158224c43ec4497b7715f6c313
Fabien Arcellier <farcellier@octo.com>
2019-12-29 22:48:22 +0100

    refact: move SpikeStarter from __main__ to spike_starter module

--- spike_starter/__main__.py
+++ spike_starter/__main__.py
@@ -3,13 +3,10 @@
 import getopt
 import logging
 import os
-import shutil
 import sys
-import tempfile
 
-from datetime import datetime
 
-import git
+from spike_starter.spike_starter import SpikeStarter
 
 
 def cli():
@@ -46,8 +43,6 @@ def main(argv):
       # Initialize git repository
       spike_starter.create_git_local_repository(project_directory)
 
-
-
   except SystemExit:
     sys.exit(1)
 
@@ -58,74 +53,6 @@ def main(argv):
 
 def usage():
   print("python %s (-h) [-t template_path] projects" % (sys.argv[0]))
-
-
-class SpikeStarter:
-
-  def __init__(self):
-    self.logger = logging.getLogger('spike-starter')
-
-  def get_project_path(self, project_name):
-    # project id from today date
-    now = datetime.now()
-    project_id = "{0:d}{1:0>2d}{2:0>2d}_{3:0>2d}{4:0>2d}".format(
-      now.year, now.month, now.day, now.hour, now.minute)
-    log_debug("project_id: {}".format(project_id))
-    return "{}__{}".format(project_id, project_name)
-
-  def create_project_directory(self, path):
-    if not os.path.exists(path):
-      os.makedirs(path)
-      log_information("PROJECT DIRECTORY : {} [OK]".format(path))
-    else:
-      log_alert("Project already exits %s" % path)
-      sys.exit(1)
-
-  def create_git_local_repository(self, path):
-    git.Repo.init(path)
-    log_information("PROJECT GIT REPOSITORY : {} [OK]".format(path))
-
-  def import_template_directory(self, destination: str, source: str) -> None:
-    blueprint_origin = self._blueprint_origin(source)
-
-    self.logger.debug('import_template_directory - blueprint_origin:%s source:%s destination:%s'
-                      , blueprint_origin, source, destination)
-
-    if blueprint_origin == 'local':
-      shutil.copytree(source, destination, ignore=shutil.ignore_patterns('.git'))
-
-    if blueprint_origin == 'git':
-      clone_destination = tempfile.mktemp(prefix='spike_starter_')
-      git.Repo.clone_from(source, clone_destination, multi_options=['--depth=1'])
-      shutil.copytree(clone_destination, destination, ignore=shutil.ignore_patterns('.git'))
-      shutil.rmtree(clone_destination)
-
-    self.logger.debug('import_template_directory - done')
-
-  def _blueprint_origin(self, source: str) -> bool:
-    blueprint_origin = None
-    if source.endswith('.git') and source.startswith('https://'):
-      blueprint_origin = 'git'
-
-    if source.endswith('.git') and source.startswith('git@'):
-      blueprint_origin = 'git'
-
-    if os.path.exists(source):
-      blueprint_origin = 'local'
-
-    return blueprint_origin
-
-
-def log_alert(text):
-  print("[ALERT] {}".format(text))
-
-
-def log_information(text):
-  print("[INFO] ", text)
-
-
-def log_debug(text):
-  print("[DEBUG] ", text)
 
 
 if __name__ == "__main__":
---
+++ spike_starter/spike_starter.py
@@ -0,0 +1,64 @@
+import logging
+import os
+import shutil
+import tempfile
+
+from datetime import datetime
+
+import git
+
+
+class SpikeStarter:
+
+  def __init__(self):
+    self.logger = logging.getLogger('spike-starter')
+
+  def get_project_path(self, project_name):
+    # project id from today date
+    now = datetime.now()
+    project_id = "{0:d}{1:0>2d}{2:0>2d}_{3:0>2d}{4:0>2d}".format(
+      now.year, now.month, now.day, now.hour, now.minute)
+    self.logger.debug("project_id:%s", project_id)
+    return "{}__{}".format(project_id, project_name)
+
+  def create_project_directory(self, path):
+    if not os.path.exists(path):
+      os.makedirs(path)
+      self.logger.info("PROJECT DIRECTORY :%s [OK]", path)
+    else:
+      self.logger.info("Project already exits %s", path)
+      raise OSError()
+
+  def create_git_local_repository(self, path):
+    git.Repo.init(path)
+    self.logger.info("PROJECT GIT REPOSITORY : %s [OK]", path)
+
+  def import_template_directory(self, destination: str, source: str) -> None:
+    blueprint_origin = self._blueprint_origin(source)
+
+    self.logger.debug('import_template_directory - blueprint_origin:%s source:%s destination:%s'
+                      , blueprint_origin, source, destination)
+
+    if blueprint_origin == 'local':
+      shutil.copytree(source, destination, ignore=shutil.ignore_patterns('.git'))
+
+    if blueprint_origin == 'git':
+      clone_destination = tempfile.mktemp(prefix='spike_starter_')
+      git.Repo.clone_from(source, clone_destination, multi_options=['--depth=1'])
+      shutil.copytree(clone_destination, destination, ignore=shutil.ignore_patterns('.git'))
+      shutil.rmtree(clone_destination)
+
+    self.logger.debug('import_template_directory - done')
+
+  def _blueprint_origin(self, source: str) -> [str]:
+    blueprint_origin = None
+    if source.endswith('.git') and source.startswith('https://'):
+      blueprint_origin = 'git'
+
+    if source.endswith('.git') and source.startswith('git@'):
+      blueprint_origin = 'git'
+
+    if os.path.exists(source):
+      blueprint_origin = 'local'
+
+    return blueprint_origin
